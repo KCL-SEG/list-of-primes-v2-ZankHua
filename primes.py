f28b660694f590bc0893ddd90887414d0b3ad80c
ZankHua <146014196+ZankHua@users.noreply.github.com>
2023-11-10 16:54:30 +0000

    new version

--- primes.py
+++ primes.py
@@ -1,11 +1,5 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def is_prime(n):
     """Check if a number is a prime number."""
-    if number_of_primes <= 0:
-        raise ValueError("number_of_primes must be a positive integer")
-    
     if n <= 1:
         return False
     if n <= 3:
@@ -21,7 +15,8 @@ def is_prime(n):
 
 def primes(number_of_primes):
     """Return a list of prime numbers of length number_of_primes."""
-    
+    if number_of_primes <= 0:
+        raise ValueError("number_of_primes must be a positive integer")
     list_of_primes = []
     num = 2  # Starting from the first prime number
     while len(list_of_primes) < number_of_primes:
@@ -30,7 +25,5 @@ def primes(number_of_primes):
         num += 1
     return list_of_primes
 
-
-
-
-
+# Example usage:
+# print(primes(10))
